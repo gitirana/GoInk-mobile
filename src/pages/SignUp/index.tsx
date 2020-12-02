9a5e8e9c275e7f1fe97c1df7c77cfa79d70fad5c
gitirana <tlgc@poli.br>
2020-12-02 20:36:58 -0300

    finishing sign up page

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,9 +1,64 @@
 import React from 'react';
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  View,
+  ScrollView,
+} from 'react-native';
 
-import { Container } from './styles';
+import Icon from 'react-native-vector-icons/Feather';
+
+import { Container, Title, BackToSignIn, BackToSignInText } from './styles';
+
+import logo from '../../assets/logo.png';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 const SignUp: React.FC = () => {
-  return <Container />;
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          contentContainerStyle={{ flex: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <Container>
+            <Image source={logo} />
+
+            <View>
+              <Title>Faça seu cadastro</Title>
+            </View>
+
+            <Input name="name" icon="user" placeholder="Nome" />
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button
+              onPress={() => {
+                console.log('apertou');
+              }}
+            >
+              Entrar
+            </Button>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+      <BackToSignIn
+        onPress={() => {
+          console.log('create acc');
+        }}
+      >
+        <Icon name="arrow-left" size={20} color="#fff" />
+        <BackToSignInText>Voltar para login</BackToSignInText>
+      </BackToSignIn>
+    </>
+  );
 };
 
 export default SignUp;
